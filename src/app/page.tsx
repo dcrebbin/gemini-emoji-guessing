373af150aff5b0d676ba98d69e081e38516e4313
Devon Crebbin <devon@artvuu.group>
2024-05-23 07:21:58 +1000

    added cool background

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-8">
+    <main className="flex min-h-screen flex-col items-center justify-between p-8 bg-no-repeat bg-cover bg-[url('/images/ffflux.svg')]">
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 direct grid-flow-col-row w-full h-full">
         <div className="w-full flex flex-col items-center gap-4">
           <h1>🤔🤔🤔🤔.ws</h1>
@@ -17,7 +17,7 @@ export default function Home() {
           <Image alt="emoji keyboard" src="/images/emoji-keyboard.jpg" width={250} height={200} />
         </div>
       </div>
-      <div className="w-full flex flex-col gap-4 justify-between">
+      <div className="w-full flex flex-col gap-4 justify-between text-gray-900">
         <p className="flex gap-1">
           by
           <a className="underline" href="https://dcrebb.in">
