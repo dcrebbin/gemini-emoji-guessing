2eeefe72cb9fa6a6254a3eb3486b4920f84acc81
Devon Crebbin <devon@artvuu.group>
2024-05-23 08:19:54 +1000

    Added initial image identification request

---
+++ src/app/api/identify/route.ts
@@ -0,0 +1,23 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+export async function GET(req: Request) {
+  const { imageData } = await req.json();
+  const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY as string);
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest" });
+  const prompt = "Find the closest matching emoji to this image. Only one emoji will be returned. If no emoji is found, the response will be empty.";
+  const image = {
+    inlineData: {
+      data: imageData,
+      mimeType: "image/jpg",
+    },
+  };
+
+  const result = await model.generateContent([prompt, image]);
+  console.log(result.response.text());
+
+  return new Response(result.response.text(), {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
