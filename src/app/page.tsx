aec487ac62099c818fd6b7d8c077ff2013d3a623
Devon Crebbin <devon@artvuu.group>
2024-05-23 09:02:13 +1000

    fixed request and added count down

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 "use client";
 import CameraIcon from "@/icons/camera";
 import StopIcon from "@/icons/stop";
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export default function Home() {
   const [video, setVideo] = useState<HTMLVideoElement | null>(null);
   const [emoji, setEmoji] = useState("❔");
   const [cameraOn, setCameraOn] = useState(false);
-
+  const [count, setCount] = useState(3);
+  const [takingPhoto, setTakingPhoto] = useState(false);
+  const overlayRef = useRef<HTMLDivElement | null>(null);
+  let photoTaken = false;
   function activateCamera() {
     navigator.mediaDevices
       .getUserMedia({ video: true, audio: false })
@@ -22,13 +25,40 @@ export default function Home() {
       });
   }
 
+  function photoCountDown() {
+    photoTaken = false;
+    setTakingPhoto(true);
+    const countDown = setInterval(() => {
+      setCount((prev) => {
+        if (prev === 0) {
+          takePhoto();
+          clearInterval(countDown);
+          return 3;
+        }
+        overlayRef.current!.style.backgroundColor = "white";
+        setTimeout(() => {
+          overlayRef.current!.style.backgroundColor = "transparent";
+        }, 100);
+        return prev - 1;
+      });
+    }, 1000);
+  }
+
   async function takePhoto() {
+    if (photoTaken) {
+      return;
+    }
+    photoTaken = true;
     const canvas = document.createElement("canvas");
     canvas.width = video!.videoWidth;
     canvas.height = video!.videoHeight;
     canvas.getContext("2d")!.drawImage(video!, 0, 0, canvas.width, canvas.height);
     const data = canvas.toDataURL("image/png");
-    console.log(data);
+    console.log("Photo taken!");
+
+    const base64Image = data.split(",")[1];
+    setTakingPhoto(false);
+    identifyEmoji(base64Image);
   }
 
   function copyEmoji() {
@@ -48,8 +78,8 @@ export default function Home() {
       console.error(`An error occurred: ${err}`);
     })) as Response;
 
-    const returnedEmoji = (await emojiResponse.json()) as string;
-    if (returnedEmoji !== "" && returnedEmoji.length === 1) {
+    const returnedEmoji = (await emojiResponse.text()) as string;
+    if (returnedEmoji !== "") {
       setEmoji(returnedEmoji);
       console.log(`The emoji is: ${returnedEmoji}`);
     } else {
@@ -74,13 +104,15 @@ export default function Home() {
         <div className="w-full flex flex-col items-center gap-4 order-1 lg:order-2">
           <h1 className="text-2xl lg:text-4xl">🤔🤔🤔🤔.ws</h1>
           <div className="bg-white/30 w-full md:h-[17rem] lg:h-[30rem] h-60 rounded-lg flex items-center justify-center relative drop-shadow-md">
+            <div ref={overlayRef} className="w-full h-full absolute"></div>
             <video ref={(video) => setVideo(video)} className="w-full h-full object-cover rounded-lg" autoPlay playsInline />
+            <div className="absolute top-0 left-0 w-full h-full flex items-center justify-center">{count > 0 && takingPhoto ? <p className="text-[8rem] text-black/50">{count}</p> : null}</div>
             <div className="flex items-center absolute bottom-0 gap-2 mb-2">
               <button
                 className="w-16 h-16 bg-black/60 rounded-2xl drop-shadow-lg"
                 onClick={() => {
-                  if (video?.srcObject) {
-                    takePhoto();
+                  if (video?.srcObject && count === 3) {
+                    photoCountDown();
                   } else {
                     activateCamera();
                   }
@@ -96,7 +128,7 @@ export default function Home() {
             </div>
           </div>
           <div className="flex flex-col">
-            <input className="w-24 text-center bg-none text-[4rem] rounded-t-lg" value={emoji}></input>
+            <input className="w-24 text-center bg-none text-[4rem] rounded-t-lg" readOnly={true} value={emoji}></input>
             <button onClick={copyEmoji} className="w-24 text-3xl bg-black rounded-b-lg">
               COPY
             </button>
