c75af248c522bb9bca1aa0e1f62f9a2fbc2b800a
Devon Crebbin <devon@artvuu.group>
2024-05-23 11:37:21 +1000

    fixed icon errors

--- src/icons/camera.tsx
+++ src/icons/camera.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 export default function CameraIcon() {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" className="size-6 m-4">
--- src/icons/github.tsx
+++ src/icons/github.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 export default function Github() {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="white" className="size-6 m-4 text-white">
