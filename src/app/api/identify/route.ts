e9c6cc6990576312895f97c999be7710412148cc
Devon Crebbin <devon@artvuu.group>
2024-05-24 16:17:10 +1000

    updated model

--- src/app/api/identify/route.ts
+++ src/app/api/identify/route.ts
@@ -3,7 +3,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 export async function POST(req: Request) {
   const { imageData } = await req.json();
   const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY as string);
-  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest" });
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro-latest" });
   const prompt = "Find the closest matching emoji. Prioritize human expressions (e.g:😬🤨🥴🤠) and body language (e.g: 🤷🙅‍♂️🧏‍♀️🙋‍♂️💁‍♂️🙇‍♂️). Only one emoji will be returned. If no emoji is found, the response will be empty.";
   const image = {
     inlineData: {
